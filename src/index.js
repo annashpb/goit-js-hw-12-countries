3a8e7eb9d2185d516ff753b6bdde97c5734f6544
Anna Sh <anna.sh.pb@gmail.com>
2019-12-06 00:28:56 +0200

    mod12

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import './styles.css';
 import fetchCountries from './js/components/fetchCountries';
 import searchResultsListItemsTemplate from './js/templates/searchresults-list-items.hbs';
+import countryDescriptionTemplate from './js/templates/country-description.hbs';
+import PNotify from '../node_modules/pnotify/dist/es/PNotify'
+import PNotifyStyleMaterial from '../node_modules/pnotify/dist/es/PNotifyStyleMaterial.js';
+PNotify.defaults.styling = 'material';
+PNotify.defaults.icons = 'material';
+// PNotify.defaults = {
+//     styling: 'bootstrap3',
+//     icons: 'material'
+// }
 
 const debounce = require('lodash.debounce');
 const refs = {
   searchForm: document.querySelector('.input-js'),
   searchResultsList: document.querySelector('.search-results'),
+  contryDescription: document.querySelector('.country-description-js'),
 };
 
 function searchFormSubmitHandler(event) {
   event.preventDefault();
-  const form = event.currentTarget;
-  const inputValue = refs.searchForm.value.toLowerCase();
-  console.log(inputValue);
+  const form = event.target;
+  const inputValue = form.value.toLowerCase();
   fetchCountries(inputValue)
     .then(countries =>
       countries.filter(country =>
@@ -20,9 +29,20 @@ function searchFormSubmitHandler(event) {
       ),
     )
     .then(result => {
-      const markup = buildListItemsMarkup(result);
-      insertListItems(markup);
-    });
+      const resultArr = Array.from(result);
+      if (resultArr.length === 1) {
+        const descriptionMarkup = buildCountryDescriptionMarkup(result[0]);
+        insertCountryDescription(descriptionMarkup);
+      } else if (resultArr.length > 1 && resultArr.length <= 10) {
+        const listMarkup = buildListItemsMarkup(result);
+        insertListItems(listMarkup);
+      } else {
+        PNotify.error({
+            text: "Too many matches found! Please, enter a more specific query"
+          });
+      }
+    })
+    .catch(err => console.warn(err));
 }
 
 function buildListItemsMarkup(items) {
@@ -33,4 +53,21 @@ function insertListItems(items) {
   refs.searchResultsList.insertAdjacentHTML('beforeend', items);
 }
 
-refs.searchForm.addEventListener('input', searchFormSubmitHandler);
+function buildCountryDescriptionMarkup(item) {
+  return countryDescriptionTemplate(item);
+}
+
+function insertCountryDescription(item) {
+  refs.contryDescription.insertAdjacentHTML('afterbegin', item);
+}
+
+function clearListItems() {
+  refs.searchResultsList.innerHTML = '';
+  refs.contryDescription.innerHTML = '';
+}
+
+refs.searchForm.addEventListener(
+  'input',
+  debounce(searchFormSubmitHandler, 500),
+);
+refs.searchForm.addEventListener('blur', clearListItems);
--- src/js/components/fetchCountries.js
+++ src/js/components/fetchCountries.js
@@ -1,5 +1,4 @@
 const baseUrl = 'https://restcountries.eu/rest/v2/';
-const resourse = 'name/';
 
 export default function fetchCountries(searchQuery) {
   return fetch(baseUrl).then(response => response.json());
